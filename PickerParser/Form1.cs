48c26da561a482ded1e6e84fa00ded8d865e3a88
Anton <xpe6e3@gmail.com>
2023-09-07 12:03:20 +0600

    Add project files.

---
+++ PickerParser/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace PickerParser
+{
+    public partial class Form1 : Form
+    {
+
+        string baseUrl = "https://ru.pickgamer.com/games";
+        string pageContent = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            using (WebClient wc = new WebClient())
+            {
+                wc.Encoding = Encoding.UTF8;
+                pageContent = wc.DownloadString(baseUrl);
+            }
+
+         
+
+        }
+
+        async void ParseGameNames()
+        {
+            string regexGame = @"<a href=""https://ru.pickgamer.com/games/(.*?)\/requirements""";
+            MatchCollection matches = Regex.Matches(pageContent, regexGame);
+
+            List<string> games = new List<string>();
+            foreach (Match match in matches)
+                games.Add(match.Groups[1].ToString());
+        }
+
+
+    }
+}
