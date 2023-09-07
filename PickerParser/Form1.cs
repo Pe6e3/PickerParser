a53393884bbf26035902fa7512423a397c86a032
Anton <xpe6e3@gmail.com>
2023-09-07 12:25:29 +0600

    Настроил методы получения текста страницы и извлечения нзваний игр, которые применяются в ссылках

--- PickerParser/Form1.cs
+++ PickerParser/Form1.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PickerParser
@@ -17,29 +19,31 @@ namespace PickerParser
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        async void button1_Click(object sender, EventArgs e)
         {
-            
+            await GetPageContent();
+            ParseGameNames();
+        }
+
+        async Task GetPageContent()
+        {
             using (WebClient wc = new WebClient())
             {
                 wc.Encoding = Encoding.UTF8;
-                pageContent = wc.DownloadString(baseUrl);
+                pageContent = await wc.DownloadStringTaskAsync(baseUrl);
             }
-
-         
-
         }
-
-        async void ParseGameNames()
+        void ParseGameNames()
         {
             string regexGame = @"<a href=""https://ru.pickgamer.com/games/(.*?)\/requirements""";
             MatchCollection matches = Regex.Matches(pageContent, regexGame);
 
-            List<string> games = new List<string>();
+            HashSet<string> games = new HashSet<string>();
             foreach (Match match in matches)
                 games.Add(match.Groups[1].ToString());
-        }
 
+            output.Text = string.Join("\n", games); ///////////////////////
+        }
 
     }
 }
